1810fe87fd781603c68943586c30ca10051c93a5
Dolue <arinzechukwudolue@gmail.com>
2022-08-04 13:27:38 +0100

    second commit

--- app.js
+++ app.js
@@ -41,16 +41,11 @@ app.use(cors())
 app.use(xss())
 app.use(mongoSanitize())
 
-app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("./public"));
 app.use(fileUpload());
-
-app.get("/", (req, res) => {
-  res.send("e-commerce API");
-});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
